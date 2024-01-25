b8c6b01147c6b8f938061ba83006afbedcdd0ddd
Gary Núñez <42857543+gary185@users.noreply.github.com>
2024-01-24 22:28:17 -0500

    Update holaMundo.py

--- holaMundo.py
+++ holaMundo.py
@@ -1,1 +1,11 @@
-print("HOLA MUNDO")
+#print("HOLA MUNDO")
+import os
+
+
+def main():
+    nombre = os.getenv("USERNAME")
+    print(f"¡Hola, {nombre} desde GitHub!")
+
+
+if __name__ == "__main__":
+    main()
